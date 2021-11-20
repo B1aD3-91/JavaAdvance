eb1c3ba8f6f8dd78578999591c8dc7f51a8c3c0b
B1aD3-91 <blade91@ukr.net>
2021-11-20 20:03:12 +0200

    Home Work #6

--- src/main/java/HomeWork5_2/McDuck/McDuck.java
+++ src/main/java/HomeWork5_2/McDuck/McDuck.java
@@ -3,29 +3,25 @@ package HomeWork5_2.McDuck;
 import lombok.SneakyThrows;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class McDuck {
 
     private static final int MIN_SERVICE_TIME = 3;
     private static final int MAX_SERVICE_TIME = 8;
     private final Random rnd = new Random();
-    private boolean isFree = true;
+    private final Object lock = new Object();
 
     @SneakyThrows
-    public synchronized void freeCashier() {
-        if (!isFree) {
-            System.out.printf("Customer %s is wait%n", Thread.currentThread().getId());
-            wait();
+    public void freeCashier() {
+        synchronized (lock) {
+            if (closeTime()) {
+               System.out.println("Time to close. Cashier is closing.");
+              System.exit(0);
+            }
+            System.out.printf("Customer %s is service%n", Thread.currentThread().getId());
+            TimeUnit.SECONDS.sleep(randomServiceTime());
         }
-        if (closeTime()) {
-            System.out.println("Time to close. Cashier is closing.");
-            System.exit(0);
-        }
-        System.out.printf("Customer %s is service%n", Thread.currentThread().getId());
-        isFree = false;
-        wait(1000L * randomServiceTime());
-        isFree = true;
-        notify();
     }
 
     private int randomServiceTime() {
