2a630620919d90e105c0b29da8ff59d46f81f8d6
B1aD3-91 <blade91@ukr.net>
2021-11-24 22:04:32 +0200

    Add home work #8

---
+++ src/main/resources/dump.sql
@@ -0,0 +1,60 @@
+CREATE TABLE Heroes
+(
+    id       INT IDENTITY (1, 1),
+    name     VARCHAR(255),
+    ultimate VARCHAR(255)
+);
+GO
+INSERT INTO Heroes(name, ultimate)
+VALUES ('Abaddon(Абадон)', '');
+GO
+INSERT INTO Heroes(name, ultimate)
+VALUES ('Alchemist(Алхимик)', '');
+GO
+INSERT INTO Heroes(name, ultimate)
+VALUES ('Ancient Apparation(Апарат)', '');
+GO
+INSERT INTO Heroes(name, ultimate)
+VALUES ('Anti-Mage(Антимаг) - Magina(Магина)', '');
+GO
+INSERT INTO Heroes(name, ultimate)
+VALUES ('Axe(Акс) - Mogul Khan(Могул Кан)', '');
+GO
+INSERT INTO Heroes(name, ultimate)
+VALUES ('Bane(Бэйн) - Atropos(Атропос)', '');
+GO
+INSERT INTO Heroes(name, ultimate)
+VALUES ('Batrider(Бэтрайдер)', '');
+GO
+INSERT INTO Heroes(name, ultimate)
+VALUES ('Beastmaster(Бистмастер)', '');
+GO
+INSERT INTO Heroes(name, ultimate)
+VALUES ('Pudge(Пудж)', '');
+GO
+INSERT INTO Heroes(name, ultimate)
+VALUES ('Zeus(Зевс))', '');
+GO
+INSERT INTO Heroes(name, ultimate)
+VALUES ('Ancient Apparation(Апарат)', '');
+GO
+INSERT INTO Heroes(name, ultimate)
+VALUES ('Anti-Mage(Антимаг) - Magina(Магина)', '');
+GO
+INSERT INTO Heroes(name, ultimate)
+VALUES ('Axe(Акс) - Mogul Khan(Могул Кан)', '');
+GO
+INSERT INTO Heroes(name, ultimate)
+VALUES ('Bane(Бэйн) - Atropos(Атропос)', '');
+GO
+INSERT INTO Heroes(name, ultimate)
+VALUES ('Batrider(Бэтрайдер)', '');
+GO
+INSERT INTO Heroes(name, ultimate)
+VALUES ('Beastmaster(Бистмастер)', '');
+GO
+INSERT INTO Heroes(name, ultimate)
+VALUES ('Pudge(Пудж)', '');
+GO
+INSERT INTO Heroes(name, ultimate)
+VALUES ('Zeus(Зевс))', '');
